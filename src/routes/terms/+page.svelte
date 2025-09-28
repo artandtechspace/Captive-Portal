0cd3d1f3ffc2990aee641552137743ba7ca9b0d3
Luca Schöneberg <luca-schoeneberg@outlook.com>
2025-09-28 16:39:59 +0200

    feat: rebuild portal with svelte

---
+++ src/routes/terms/+page.svelte
@@ -0,0 +1,172 @@
+<svelte:head>
+  <title>WiFi-Nutzungsbedingungen ATS</title>
+</svelte:head>
+
+<div class="blur-background relative min-h-screen bg-gray-100">
+  <div class="container mx-auto max-w-2xl p-5 min-h-screen flex items-center justify-center">
+    <div class="bg-white shadow-lg rounded-lg px-8 pt-6 pb-8 mb-4">
+      <div class="flex justify-between items-center mb-6">
+        <img class="w-24" src="/images/ats-logo.png" alt="ATS Logo" />
+        <img class="w-32 justify-end" src="/images/opnsense.png" alt="OPNsense Logo" />
+      </div>
+
+      <header class="mb-10">
+        <div class="prose">
+          <h1 class="text-2xl font-medium mb-4">Nutzungsbedingungen für WLAN-Zugriff</h1>
+          <div class="mb-4">
+            <h2 class="text-xl font-semibold">1. Gegenstand und Geltungsbereich dieser Nutzungsbedingungen</h2>
+            <p>
+              Diese Nutzungsbedingungen regeln Ihre und unsere Rechte und Pflichten im Zusammenhang mit der Nutzung
+              unseres WLAN-Zugangs.
+            </p>
+          </div>
+
+          <div class="mb-4">
+            <h2 class="text-xl font-semibold">2. Unsere Leistungen</h2>
+            <ol class="list-decimal pl-5">
+              <li>
+                (1) Wir stellen Ihnen in unseren Geschäftsräumen einen Zugang zum Internet in Form eines WLAN-Zugangs
+                („Hotspot“) zur kostenlosen Nutzung zur Verfügung.
+              </li>
+
+              <li>
+                (2) Die Bereitstellung des Hotspots richtet sich nach unseren jeweiligen technischen und betrieblichen
+                Möglichkeiten. Ein Anspruch auf einen funktionsfähigen Hotspot oder eine bestimmte örtliche Abdeckung des
+                Hotspots besteht nicht.
+              </li>
+
+              <li>
+                (3) Wir gewährleisten ferner nicht, dass der Hotspot störungs- und unterbrechungsfrei genutzt werden kann.
+                Auch können wir keine Übertragungsgeschwindigkeiten gewährleisten.
+              </li>
+
+              <li>
+                (4) Wir behalten uns das Recht vor, den Zugang zum Hotspot im Falle notwendiger technischer Reparatur- und
+                Wartungsarbeiten ohne vorherige Ankündigung zu ändern, zu beschränken oder einzustellen.
+              </li>
+
+              <li>
+                (5) Es besteht kein Anspruch darauf, dass bestimmte Dienste über den Hotspot genutzt werden können. So
+                können insbesondere Port-Sperrungen vorgenommen werden. In der Regel wird das Surfen im Internet und das
+                Senden und Empfangen von E-Mails ermöglicht.
+              </li>
+            </ol>
+          </div>
+
+          <div class="mb-4">
+            <h2 class="text-xl font-semibold">3. Zugang und Nutzung</h2>
+            <ol class="list-decimal pl-5">
+              <li>
+                (1) Wir bieten unser WLAN nur für Mitarbeiter/Mitgleider/Schüler/Besondere Gäste unseres Vereins an. Es
+                handelt sich nicht um einen öffentlich zugänglichen Telekommunikationsdienst, sondern um ein internes WLAN
+                für unseren Verein.
+              </li>
+
+              <li>
+                (2) Voraussetzung für eine Nutzung ist, dass Sie sich zuvor für die Nutzung des Hotspots registrieren
+                und/oder die Geltung dieser Nutzungsbedingungen zu Beginn der Nutzung des Hotspots akzeptieren. Dies kann
+                bei Auswahl des Hotspots als WLAN-Netz im Endgerät in der Regel über ein dann abrufbares
+                Registrierungsformular oder eine Begrüßungsseite erfolgen.
+              </li>
+
+              <li>
+                (3) Es besteht kein Anspruch auf Nutzung des Hotspots. Uns steht es frei, den Zugang zum Hotspot jederzeit
+                ohne Angabe von Gründen einzuschränken oder einzustellen.
+              </li>
+
+              <li>
+                (4) Es gilt die jeweils aktuelle Fassung dieser Nutzungsbedingungen, die Ihnen bei der Anmeldung zum
+                Hotspot abrufbar gemacht wird.
+              </li>
+            </ol>
+          </div>
+
+          <div class="mb-4">
+            <h2 class="text-xl font-semibold">4. Zugangsdaten</h2>
+            <ol class="list-decimal pl-5">
+              <li>
+                (1) Sofern Sie im Zuge einer Registrierung Anmelde-Daten (wie z.B. Benutzername, Passwort, E-Mail etc.)
+                angegeben haben, sind diese von Ihnen geheim zu halten und unbefugten Dritten nicht zugänglich zu machen.
+              </li>
+              <li>
+                (2) Sollten Sie Registrierungsdaten erhalten haben, haben Sie sicher zu stellen, dass der Zugang zu und die
+                Nutzung des Hotspots mit Ihren Benutzerdaten ausschließlich durch Sie als Nutzer erfolgt. Sofern Tatsachen
+                vorliegen, die die Annahme begründen, dass unbefugte Dritte von Ihren Zugangsdaten Kenntnis erlangt haben
+                oder erlangen werden, müssen Sie uns unverzüglich informieren.
+              </li>
+              <li>
+                (3) Sie haften als Nutzer für jedwede Nutzung und/oder sonstige Aktivität, die unter Ihren Zugangsdaten
+                ausgeführt wird, nach den gesetzlichen Bestimmungen.
+              </li>
+            </ol>
+          </div>
+
+          <div class="mb-4">
+            <h2 class="text-xl font-semibold">5. Ihre Pflichten als Nutzer</h2>
+            <ol class="list-decimal pl-5">
+              <li>
+                (1) Sie sind verpflichtet, etwaige Informationen, die im Rahmen der Nutzung des Dienstes von Ihnen zu ihrer
+                Person angegeben werden, wahrheitsgemäß zu machen.
+              </li>
+              <li>
+                (2) Sie sind verpflichtet, bei der Nutzung unseres Hotspots die geltenden Gesetze einzuhalten.
+              </li>
+              <li>
+                (3) Weitere Pflichten, die sich aus anderen Bestimmungen dieser Nutzungsbedingungen ergeben, bleiben
+                unberührt.
+              </li>
+            </ol>
+          </div>
+
+          <div class="mb-4">
+            <h2 class="text-xl font-semibold">6. Preise</h2>
+            <p>Der Dienst wird kostenlos erbracht.</p>
+          </div>
+
+          <div class="mb-4">
+            <h2 class="text-xl font-semibold">7. Verfügbarkeit der Leistungen</h2>
+            <p>
+              Da unsere Leistungen unentgeltlich erfolgen, haben Sie keinen Anspruch auf die Nutzung des Hotspots. Es
+              steht uns frei, den Zugang zum Hotspot zu jeder Zeit ohne Angabe von Gründen zu beschränken oder zu
+              beenden.
+            </p>
+          </div>
+
+          <div class="mb-4">
+            <h2 class="text-xl font-semibold">8. Verbotene Handlungen</h2>
+            <p>
+              Als Nutzer sind Sie verpflichtet, bei der Nutzung des Hotspots die geltenden Gesetze einzuhalten. Es ist
+              insbesondere untersagt, das Angebot zum Abruf oder zur Verbreitung von sitten- oder rechtswidrigen Inhalten
+              zu nutzen.
+            </p>
+          </div>
+
+          <div class="mb-4">
+            <h2 class="text-xl font-semibold">9. Verantwortlichkeit für Inhalte</h2>
+            <p>
+              Wir übernehmen keine Verantwortung für die von Ihnen über den Hotspot übermittelten oder verarbeiteten
+              Inhalte. Für diese Inhalte sind ausschließlich Sie verantwortlich.
+            </p>
+          </div>
+
+          <div class="mb-4">
+            <h2 class="text-xl font-semibold">10. Haftungsbeschränkung</h2>
+            <p>
+              Wir haften für Vorsatz und grobe Fahrlässigkeit. Für einfache Fahrlässigkeit haften wir nur bei Verletzung
+              einer wesentlichen Vertragspflicht, die zur Erreichung des Vertragszwecks erforderlich ist. Die Haftung ist
+              in diesem Fall auf den vorhersehbaren, vertragstypischen Schaden begrenzt.
+            </p>
+          </div>
+
+          <div class="mb-4">
+            <h2 class="text-xl font-semibold">11. Schlussbestimmungen</h2>
+            <p>
+              Es gilt das Recht der Bundesrepublik Deutschland. Sollten einzelne Bestimmungen dieser Nutzungsbedingungen
+              unwirksam sein oder werden, bleibt die Wirksamkeit der übrigen Bestimmungen unberührt.
+            </p>
+          </div>
+        </div>
+      </header>
+    </div>
+  </div>
+</div>
