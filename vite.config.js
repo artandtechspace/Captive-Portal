ec56b969268db0833fdbcd1882ff7ed71a438db9
Luca Schöneberg <luca-schoeneberg@outlook.com>
2025-09-28 21:00:21 +0200

    chore: set up Vitest for testing with Testing Library and jsdom environment

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,17 @@
 import {sveltekit} from '@sveltejs/kit/vite';
-import {defineConfig} from 'vite';
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from '@tailwindcss/vite';
+import {defineConfig} from 'vitest/config';
+import * as process from ".svelte-kit/ambient.js";
 
 export default defineConfig({
-    plugins: [
-        tailwindcss(),
-        sveltekit(),
-    ],
+    plugins: [tailwindcss(), sveltekit()],
+    test: {
+        environment: 'jsdom',
+        setupFiles: ['src/setupTests.ts'],
+        include: ['src/**/*.{test,spec}.{js,ts}']
+    },
+    resolve: process.env.VITEST
+        ? {
+            conditions: ['browser']
+        } : undefined
 });
